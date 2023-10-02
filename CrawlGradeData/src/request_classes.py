b0725499a84ee3bbbe529eed8794ec38c569eb69
freshpinguin <robinzufall@gmail.com>
2023-10-02 14:27:21 +0200

    Added docstrings for request classes

--- CrawlGradeData/src/request_classes.py
+++ CrawlGradeData/src/request_classes.py
@@ -7,6 +7,9 @@ warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning, module='bs4')
 from src.request_params import AuthParams, ExpandParams, GetGradesParams
 
 class AuthRequest:
+    """
+    Class contains code for doing authentification on ohmportal page.
+    """
     def __init__(self, username: str, password: str, session: requests.Session):
         self.username = username
         self.password = password
@@ -20,8 +23,12 @@ class AuthRequest:
         if failed in resp.text:
             raise Exception("Authentification failed. Try with correct credentials.")
 
+        return resp
 
 class OpenGradeViewRequest:
+    """
+    Class contains request to visit grade view on ohmportal. Necessary to get statistiks data afterwards.
+    """
     def __init__(self, session: requests.Session):
         self.session = session
         self.url = "https://studium.ohmportal.de/qisserver/pages/sul/examAssessment/personExamsReadonly.xhtml?_flowId=examsOverviewForPerson-flow&_flowExecutionKey=e1s1"
@@ -30,6 +37,10 @@ class OpenGradeViewRequest:
         return self.session.get(self.url)
     
 class ExpandGradesRequest:
+    """
+    Class contains requestexpand grade view on ohmportal. Necessary to get statistiks data afterwards.
+    Also provides specifiers and numbers to acess the data for single exams afterwards.
+    """
     def __init__(self, session: requests.Session):
         self.session = session
         self.url = 'https://studium.ohmportal.de/qisserver/pages/sul/examAssessment/personExamsReadonly.xhtml'
@@ -46,12 +57,17 @@ class ExpandGradesRequest:
         return resp
     
     def get_specifiers(self) -> List[str]:
-
+        """
+        Get specifier for every single existing exam, necessary to query the data afterwards.
+        """
         soup = BeautifulSoup(self.resp_text,features="lxml")
         buttons = soup.findAll(lambda tag: tag.name=="button" and tag['title'] == "Klassenspiegel anzeigen")
         return [button['id'] for button in buttons]
     
     def get_exam_numbers(self, specifiers: List[str]) -> List[str]:
+        """
+        Get exam numbers for every specifiers. 
+        """
         soup = BeautifulSoup(self.resp_text,features="lxml")
         specifiers = [":".join(spec.split(":")[4:-2]) for spec in specifiers]
         find_func = lambda spec, tag: tag.name=="span" and  tag.has_attr('id') and "elementnr" in tag['id'] and spec in tag['id']
@@ -60,6 +76,9 @@ class ExpandGradesRequest:
     
 
 class GetGradesRequest:
+    """
+    Class to query the data for a single exam and extracts the div containting the data.
+    """
     def __init__(self, session: requests.Session):
         self.session = session
         self.url = 'https://studium.ohmportal.de/qisserver/pages/sul/examAssessment/personExamsReadonly.xhtml'
@@ -70,6 +89,9 @@ class GetGradesRequest:
         return resp
     
     def get_data(self) -> list:
+        """
+        Extracts the div cotaining statistics data.
+        """
         soup = BeautifulSoup(self.resp_text, features="lxml")
         return soup.findAll(lambda tag: tag.name=="div" and tag.has_attr('id') and  tag['id']=="graphPlaceholder")[0]
     
